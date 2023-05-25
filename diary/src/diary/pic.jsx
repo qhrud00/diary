2030c3c110ad83dd3194938ac2ccef2a818a7c11
qhrud00 <krr1355@naver.com>
2023-05-25 14:33:03 +0900

    fix:필요없는 코드 수정

--- diary/src/diary/pic.jsx
+++ diary/src/diary/pic.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
-
-// function Pic() {
-//   const navigate = useNavigate();
-
-//   const handleTitleClick = () => {
-//     navigate("/Main");
-//   };
-
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         height: "100vh",
-//       }}
-//     >
-//       <h1
-//         style={{
-//           marginTop: 0,
-//           position: "absolute",
-//           top: 30,
-//           cursor: "pointer",
-//         }}
-//         onClick={handleTitleClick}
-//       >
-//         My Diary
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default Pic;
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
